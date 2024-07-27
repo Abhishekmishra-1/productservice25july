ef09a1f9a906c0d60ab2404e28f4e82886e5417a
root <root@LAPTOP-ULCV69I2.localdomain>
2024-07-27 20:36:24 +0530

    Initial commit

---
+++ src/main/java/com/scaler/productservice25july/services/ProductServiceDBImpl.java
@@ -0,0 +1,12 @@
+package com.scaler.productservice25july.services;
+
+import com.scaler.productservice25july.models.Product;
+import org.springframework.stereotype.Service;
+
+@Service("dbProcuctService")
+public class ProductServiceDBImpl implements ProductService{
+    @Override
+    public Product createProduct(Product product) {
+        return null;
+    }
+}
